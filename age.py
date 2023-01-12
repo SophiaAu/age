f9db777907bd0633479475aa05b6b53e85b34512
Sophia Ou <danojl@sina.com>
2023-01-12 15:56:45 +0800

    finished age.py

---
+++ age.py
@@ -0,0 +1,18 @@
+driving = input('请问你有没有开过车？')
+if driving != '有' and driving != '没有':
+	print('只能输入 有/没有')
+
+age = input('请问你的年龄？')
+age = int(age)
+if driving == '有':
+    if age >= 18:
+    	print('你通过测验了')
+    else:
+        print('奇怪 你怎么会有开过车')
+elif driving == '没有':
+    if age >= 18:
+    	print('你可以去考驾照了啊，怎么还不去考')
+    else:
+    	print('很好 再过几年就可以去考驾照了')
+
+	   
